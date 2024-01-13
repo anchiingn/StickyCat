f5b016befb289e48adffe2501bc3d47f61454d62
Anchi <anchi.n248@gmail.com>
2024-01-13 17:32:41 -0600

    work on css

--- react-vite/src/components/ExtraPages/LaunchStickers.jsx
+++ react-vite/src/components/ExtraPages/LaunchStickers.jsx
@@ -5,9 +5,22 @@ import './ExtraPages.css'
 export default function LaunchStickers () {
     return (
         <>
-        <NavLink to="/new-sticker">
-            Create your own Sticky Sticker
-        </NavLink>
+        <div className='image-backgrround'>
+            <img src="./src/images/IMG_2901.JPG" alt=""/>
+        </div>
+        <div className='blank-background_container'>
+            <div className='small_container'>Innovation <span>-</span> Imaginative <span>-</span> Originality <span>-</span> Inventive <span>-</span> Visionary</div>
+            <div className='about-infos_container'>
+                <h1>TURN YOUR CREATIVITY INTO STICKER</h1>
+                <p>Bringing your dream sticker collection to life has never been more seamless! StickyCat partners with you to conceptualize, present, market, and manufacture a unique sticker series tailored by you, exclusively for your fans! And guess what? It's a worry-free journey with no upfront expenses!</p>
+                <div className="line-in-between"></div>
+
+                <button className='user-button'>
+                    <NavLink to="/new-sticker" className={'navlink'} style={{padding:'20px 40px', borderRadius:'50px'}}> Let Create Sticker</NavLink>
+                </button>
+            </div>
+        </div>
+        <div>hello</div>
         </>
     )
 }
--- react-vite/src/components/ExtraPages/MainPage.jsx
+++ react-vite/src/components/ExtraPages/MainPage.jsx
@@ -3,9 +3,7 @@ import './ExtraPages.css'
 export default function MainPage () {
     return (
         <>
-            <div style={{width:'50em', height:'100%'}}>
-                <img src="./src/images/IMG_2901.JPG" alt="" style={{objectFit:'contain', width:'50em'}}/>
-            </div>
+            
         </>
     )
 }
