c9f9b18a1ea0e764b34c508d6ad066f902b3843c
Anchi <anchi.n248@gmail.com>
2024-05-11 22:19:46 -0500

    ...

--- app/models/user.py
+++ app/models/user.py
@@ -12,7 +12,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(40), nullable=False)
     lastname = db.Column(db.String(40), nullable=False)
-    username = db.Column(db.String(40), nullable=False, unique=True)
+    username = db.Column(db.String(40), unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
