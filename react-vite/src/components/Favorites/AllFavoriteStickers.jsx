322661cb4b6aefe1593a002ed446bd3efbf5ef81
Anchi <anchi.n248@gmail.com>
2024-06-08 13:39:38 -0500

    fix explore sticker page

--- react-vite/src/components/Favorites/AllFavoriteStickers.jsx
+++ react-vite/src/components/Favorites/AllFavoriteStickers.jsx
@@ -39,51 +39,53 @@ export default function AllFavoriteStickers() {
                 /
             </div>
 
-        <div className="allStickers_container" id="user-stickers_container">
-            {favorite_stickers.length === 0 ?(
-                    <>
-                    <div className="user-stickers_container"> 
-                        {/* <div style={{fontFamily:'var(--big-font)', fontSize:'30px', letterSpacing:'1.25px'}}>My Favorite Sticky Stickers</div> */}
-                        <p className="user-paragraph">Oops! Your sticker collection seems a bit bare. Let&apos;s explored all charming masterpiece together!</p>
-                        <div className="line-in-between"></div>
-                        <button className="user-button">
-                            <NavLink to={'/explored-stickers'} className={'navlink'} style={{padding:'20px 40px', borderRadius:'50px'}}>Explore Sticker</NavLink>
-                        </button>
-                    </div>
-                    </>
-                ) :(
-                    <>
-                    {/* <div style={{fontFamily:'var(--big-font)', fontSize:'30px', letterSpacing:'1.25px'}}>My Favorite Sticky Stickers</div>
-                    <div className="line-in-between"></div> */}
-                        <div className="sticker-cards_container">
-                            {favorite_stickers.map(sticker => {
-                                // -------------------  Star Rating  ------------------- //
-                                // let starRating= 0;
-                                // if (sticker.stickers && sticker.stickers.length > 0 ) { //check if it their is review before iteration
-                                // for (let star of sticker.stickers[0]?.star) {
-                                //     starRating += (star) / sticker.stickers[0]?.star?.length;
-                                // }
-                                // }
-                                // Check if sticker and sticker.stickers exist and have at least one element
-                                if (sticker && sticker.stickers && sticker.stickers.length > 0 && sticker.userId === user.id) {
-                                return (
-                                    <div key={sticker?.id} className="stickers_container">
-                                        <StickerCards sticker={sticker.stickers[0]} />
-                                        <div className="sticker-details_bottom">
-                                            <div></div>
-                                            <div style={{listStyle:'none', cursor:'pointer'}}>
-                                                <OpenModalMenuItem
-                                                itemText='Delete'
-                                                modalComponent={<DeleteFavorites sticker={sticker} />}
-                                                />
-                                            </div>
-                                        </div>
-                                        <AddToCart sticker={sticker.stickers[0]} />
-                                    </div>
-                                )} })}
+        <div style={{ display:'flex', justifyContent:'center'}}>
+            <div className="allStickers_container" id="user-stickers_container">
+                {favorite_stickers.length === 0 ?(
+                        <>
+                        <div className="user-stickers_container"> 
+                            {/* <div style={{fontFamily:'var(--big-font)', fontSize:'30px', letterSpacing:'1.25px'}}>My Favorite Sticky Stickers</div> */}
+                            <p className="user-paragraph">Oops! Your sticker collection seems a bit bare. Let&apos;s explored all charming masterpiece together!</p>
+                            <div className="line-in-between"></div>
+                            <button className="user-button">
+                                <NavLink to={'/explored-stickers'} className={'navlink'} style={{padding:'20px 40px', borderRadius:'50px'}}>Explore Sticker</NavLink>
+                            </button>
                         </div>
-                    </>
-                )}
+                        </>
+                    ) :(
+                        <>
+                        {/* <div style={{fontFamily:'var(--big-font)', fontSize:'30px', letterSpacing:'1.25px'}}>My Favorite Sticky Stickers</div>
+                        <div className="line-in-between"></div> */}
+                            <div className="sticker-cards_container">
+                                {favorite_stickers.map(sticker => {
+                                    // -------------------  Star Rating  ------------------- //
+                                    // let starRating= 0;
+                                    // if (sticker.stickers && sticker.stickers.length > 0 ) { //check if it their is review before iteration
+                                    // for (let star of sticker.stickers[0]?.star) {
+                                    //     starRating += (star) / sticker.stickers[0]?.star?.length;
+                                    // }
+                                    // }
+                                    // Check if sticker and sticker.stickers exist and have at least one element
+                                    if (sticker && sticker.stickers && sticker.stickers.length > 0 && sticker.userId === user.id) {
+                                    return (
+                                        <div key={sticker?.id} className="stickers_container">
+                                            <StickerCards sticker={sticker.stickers[0]} />
+                                            <div className="sticker-details_bottom">
+                                                <div></div>
+                                                <div style={{listStyle:'none', cursor:'pointer'}}>
+                                                    <OpenModalMenuItem
+                                                    itemText='Delete'
+                                                    modalComponent={<DeleteFavorites sticker={sticker} />}
+                                                    />
+                                                </div>
+                                            </div>
+                                            <AddToCart sticker={sticker.stickers[0]} />
+                                        </div>
+                                    )} })}
+                            </div>
+                        </>
+                    )}
+            </div>
         </div>
 
         {/* </div> */}
--- react-vite/src/components/Stickers/CurrentStickers.jsx
+++ react-vite/src/components/Stickers/CurrentStickers.jsx
@@ -39,55 +39,58 @@ export default function CurrentStickers() {
                                 <span>My Stickers</span> <span>/</span>
                             </div>
 
-                            <div className="allStickers_container">
-                                {stickers.length === 0 ? (
-                                    <>
-                                        <div className="user-stickers_container">
+                            <div style={{ display:'flex', justifyContent:'center'}}>
+                                <div className="allStickers_container">
+                                    {stickers.length === 0 ? (
+                                        <>
+                                            <div className="user-stickers_container">
+                                                {/* <div style={{fontFamily:'var(--big-font)', fontSize:'30px', letterSpacing:'1.25px'}}>My Sticky Stickers</div> */}
+                                                <p className="user-paragraph">Oops! Your sticker collection seems a bit bare. Let's create a charming masterpiece together!</p>
+                                                <div className="line-in-between"></div>
+                                                <button className="user-button">
+                                                    <NavLink to={'/new-sticker'} className={'navlink'} style={{ padding: '20px 40px', borderRadius: '50px' }}>Create Sticker</NavLink>
+                                                </button>
+                                            </div>
+                                        </>
+                                    ) : (
+                                        <>
                                             {/* <div style={{fontFamily:'var(--big-font)', fontSize:'30px', letterSpacing:'1.25px'}}>My Sticky Stickers</div> */}
-                                            <p className="user-paragraph">Oops! Your sticker collection seems a bit bare. Let's create a charming masterpiece together!</p>
-                                            <div className="line-in-between"></div>
-                                            <button className="user-button">
-                                                <NavLink to={'/new-sticker'} className={'navlink'} style={{ padding: '20px 40px', borderRadius: '50px' }}>Create Sticker</NavLink>
-                                            </button>
-                                        </div>
-                                    </>
-                                ) : (
-                                    <>
-                                        {/* <div style={{fontFamily:'var(--big-font)', fontSize:'30px', letterSpacing:'1.25px'}}>My Sticky Stickers</div> */}
-                                        {/* <div className="line-in-between"></div> */}
-                                        <div className="sticker-cards_container">
-                                            {stickers.map(sticker => {
+                                            {/* <div className="line-in-between"></div> */}
+                                            <div className="sticker-cards_container">
+                                                {stickers.map(sticker => {
 
-                                                // -------------------  Star Rating  ------------------- //
-                                                let starRating = 0;
-                                                if (sticker.reviews && sticker.reviews.length >= 1) { //check if it their is review before iteration
-                                                    for (let review of sticker.reviews) {
-                                                        starRating += (review?.star) / sticker.reviews.length;
+                                                    // -------------------  Star Rating  ------------------- //
+                                                    let starRating = 0;
+                                                    if (sticker.reviews && sticker.reviews.length >= 1) { //check if it their is review before iteration
+                                                        for (let review of sticker.reviews) {
+                                                            starRating += (review?.star) / sticker.reviews.length;
+                                                        }
                                                     }
-                                                }
 
-                                                if (user?.id === sticker?.ownerId) {
-                                                    return (
-                                                        <div key={sticker?.id} className="stickers_container">
-                                                            <StickerCards sticker={sticker} />
-                                                            <AddToCart sticker={sticker} />
-                                                            <div className="sticker-details_bottom">
-                                                                <NavLink to={`/${sticker.id}/edit-sticker`} className={'navlink'}>Edit</NavLink>
-                                                                <div style={{ listStyle: 'none', cursor: 'pointer' }}>
-                                                                    <OpenModalMenuItem
-                                                                        itemText='Delete'
-                                                                        modalComponent={<DeleteSticker sticker={sticker} />}
-                                                                    />
+                                                    if (user?.id === sticker?.ownerId) {
+                                                        return (
+                                                            <div key={sticker?.id} className="stickers_container">
+                                                                <StickerCards sticker={sticker} />
+                                                                <AddToCart sticker={sticker} />
+                                                                <div className="sticker-details_bottom">
+                                                                    <NavLink to={`/${sticker.id}/edit-sticker`} className={'navlink'}>Edit</NavLink>
+                                                                    <div style={{ listStyle: 'none', cursor: 'pointer' }}>
+                                                                        <OpenModalMenuItem
+                                                                            itemText='Delete'
+                                                                            modalComponent={<DeleteSticker sticker={sticker} />}
+                                                                        />
+                                                                    </div>
                                                                 </div>
+                                                                <AddToFavorite sticker={sticker} />
                                                             </div>
-                                                            <AddToFavorite sticker={sticker} />
-                                                        </div>
-                                                    )
-                                                }
-                                            })}
-                                        </div>
-                                    </>
-                                )}
+                                                        )
+                                                    }
+                                                })}
+                                            </div>
+                                        </>
+                                    )}
+                                </div>
+
                             </div>
                         </>
                     )}
