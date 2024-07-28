5699a40fb531f597a598979e4fd66bd8944772ac
Anchi <anchi.n248@gmail.com>
2024-07-27 19:45:17 -0500

    update

--- react-vite/src/components/Stickers/AllStickers.jsx
+++ react-vite/src/components/Stickers/AllStickers.jsx
@@ -65,6 +65,7 @@ export default function AllStickers() {
                         <div className="sort-filter">
                             <div>sort <i className="fa-solid fa-arrow-up-a-z"></i></div>
                             <div>A-Z</div>
+                        </div>
 
                         {isLoading ?(
                             <h2 className="loading">Loading...</h2>
